becb41f20bb04d2ac81079bd91e2f21a3d2902fb
Ruddy1996 <luyindularuddy@gmail.com>
2025-09-18 14:24:58 +0100

    mise à jour

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5000/api',
   //baseURL: "https://recytech-africa-production.up.railway.app/api",
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   }
