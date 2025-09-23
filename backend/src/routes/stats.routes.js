edd7973ec05ecf6f893577e3d8a145ab77e602b3
Ruddy1996 <luyindularuddy@gmail.com>
2025-09-23 11:23:10 +0100

    tout les fichier à jour

--- backend/src/routes/stats.routes.js
+++ backend/src/routes/stats.routes.js
@@ -6,7 +6,7 @@ const { authenticateToken } = require('../middlewares/auth.middleware');
 const authorize = require('../middlewares/authorize.middleware');
 
 // Stats globales (admin uniquement)
-router.get('/global', authenticateToken, StatsController.globalStats);
+router.get('/global', authenticateToken, authorize('Admin'), StatsController.globalStats);
 
 // Stats utilisateur (admin ou mobile user)
 router.get('/users/:id', authenticateToken, StatsController.userStats);
