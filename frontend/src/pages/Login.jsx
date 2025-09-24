65f8fa0541630660216cb99c198fdff438873e01
Ruddy1996 <luyindularuddy@gmail.com>
2025-09-24 09:25:22 +0100

    accès global

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -31,7 +31,7 @@ export default function LoginPage() {
       // Redirection selon le rôle
       switch (user.role) {
         case 'Admin':
-          navigate('/Admin');
+          navigate('/admin');
           break;
         case 'Autorite':
           navigate('/autorite');
