2ca1689a499a2570d09cd1b8176659e71d0a0767
Ruddy1996 <luyindularuddy@gmail.com>
2025-09-24 10:24:03 +0100

    correctionS

--- backend/src/routes/stats.routes.js
+++ backend/src/routes/stats.routes.js
@@ -6,17 +6,17 @@ const { authenticateToken } = require('../middlewares/auth.middleware');
 const authorize = require('../middlewares/authorize.middleware');
 
 // Stats globales (admin uniquement)
-router.get('/global', authenticateToken, authorize('admin'), StatsController.globalStats);
+router.get('/global', authenticateToken, authorize('Admin'), StatsController.globalStats);
 
 // Stats utilisateur (admin ou mobile user)
 router.get('/users/:id', authenticateToken, StatsController.userStats);
 
 // Stats bornes (admin)
-router.get('/bornes', authenticateToken, authorize('admin'), StatsController.statsParBorne);
+router.get('/bornes', authenticateToken, authorize('Admin'), StatsController.statsParBorne);
 
 // Stats dépôts (admin)
-router.get('/par-jour', authenticateToken, authorize('admin'), StatsController.depotsParJour);
-router.get('/depot/:id', authenticateToken, authorize('admin'), StatsController.depotDetails);
+router.get('/par-jour', authenticateToken, authorize('Admin'), StatsController.depotsParJour);
+router.get('/depot/:id', authenticateToken, authorize('Admin'), StatsController.depotDetails);
 
 
 module.exports = router;
